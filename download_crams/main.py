f35adeab1ecbbda193b93683f3846d97db8b2210
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-10-25 13:46:00 +1100

    Fix storage requirements

--- download_crams/main.py
+++ download_crams/main.py
@@ -45,8 +45,9 @@ def main():
             )
             batch.write_output(job.ofile, output_path(path_components[-1]))
             job.cpu(0.25)  # Network bandwidth is the bottleneck, not CPU.
+            job.storage('50Gi')
 
-            file_count += 1
+            file_count += 5
             if file_count > 1 and ACCESS_LEVEL == 'test':
                 break  # Only copy a subset of CRAMs for 'test'.
 
