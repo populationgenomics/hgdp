c03b6eb1e969155f1a820019a6d92a8ac0229c8d
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-10-25 12:15:54 +1100

    Initial commit

---
+++ download_crams/README.md
@@ -0,0 +1,7 @@
+# Download HGDP CRAMs from the EBI FTP server
+
+The FTP server access [requires the Aspera software](https://www.internationalgenome.org/category/ftp/), which is why this script uses the `ibmcom/aspera-cli` Docker image.
+
+```sh
+gcloud builds submit -t
+```
---
+++ download_crams/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""Copies HGDP CRAMs from EBI's FTP server."""
+
+import csv
+import gzip
+import os
+import hailtop.batch as hb
+from analysis_runner import output_path
+
+GCLOUD_AUTH = 'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
+ACCESS_LEVEL = os.getenv('ACCESS_LEVEL')
+
+
+def main():
+    """Main entry point."""
+    service_backend = hb.ServiceBackend(
+        billing_project=os.getenv('HAIL_BILLING_PROJECT'),
+        bucket=os.getenv('HAIL_BUCKET'),
+    )
+
+    batch = hb.Batch(name='HGDP CRAMs download', backend=service_backend)
+
+    with gzip.open('hgdp_sample_metadata.tsv.gz') as metadata_file:
+        reader = csv.DictReader(metadata_file, delimiter='\t')
+        file_count = 0
+        for row in reader:
+            url = row['url']
+            if not url.endswith('.cram'):
+                continue
+
+            job = batch.new_job(name=url)
+            job.image('ibmcom/aspera-cli:3.9')
+            job.command(GCLOUD_AUTH)
+            path_components = url.split('/')
+            path = '/'.join(path_components[3:])  # Remove the ftp://host/ prefix.
+            # Unfortunately, piping to stdout doesn't seem to work with ascp, so we
+            # write locally first and then delocalize using Hail Batch.
+            job.command(
+                f'ascp -i /home/aspera/.aspera/cli/etc/asperaweb_id_dsa.openssh '
+                f'-Tr -Q -l 100M -P33001 -L- '
+                f'fasp-g1k@fasp.1000genomes.ebi.ac.uk:{path} {job.ofile}'
+            )
+            batch.write_output(
+                job.ofile, output_path(f'cram/ebi/{path_components[-1]}')
+            )
+            job.cpu(0.25)  # Network bandwidth is the bottleneck, not CPU.
+
+            file_count += 1
+            if file_count > 5 and ACCESS_LEVEL == 'test':
+                break  # Only copy a subset of CRAMs for 'test'.
+
+    batch.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()
