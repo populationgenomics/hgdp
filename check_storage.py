9ab97dd7127f9ff506e99f9538e2ae4d506badd7
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-09-06 11:05:30 +1000

    Add gcloud

--- check_storage.py
+++ check_storage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from cpg_utils.workflows.batch import get_batch
+from cpg_utils.hail_batch import authenticate_cloud_credentials_in_job
 
 b = get_batch('Check HGDP storage')
 j = b.new_job('Check HGDP storage')
+j.image('hail')
+authenticate_cloud_credentials_in_job(j)
 j.command('gsutil du -sh gs://cpg-hgdp-main/gvcf/')
 j.command('gsutil du -sh gs://cpg-hgdp-main/cram/')
 j.command('gsutil du -sh gs://cpg-hgdp-main/mt/oceania.mt')
