8cf64c6bcdfcaa703f5f7273f7b263a9d3f576bc
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-09-06 12:00:52 +1000

    Use dataset_path

--- check_storage.py
+++ check_storage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 from cpg_utils.workflows.batch import get_batch
-from cpg_utils.hail_batch import authenticate_cloud_credentials_in_job, image_path
+from cpg_utils.hail_batch import authenticate_cloud_credentials_in_job, image_path, dataset_path
 
 b = get_batch('Check HGDP storage')
 j = b.new_job('Check HGDP storage')
 j.image(image_path('hail'))
 authenticate_cloud_credentials_in_job(j)
-j.command('gsutil du -sh gs://cpg-hgdp-main/gvcf/')
-j.command('gsutil du -sh gs://cpg-hgdp-main/cram/')
-j.command('gsutil du -sh gs://cpg-hgdp-main/mt/oceania.mt')
-j.command('gsutil du -sh gs://cpg-hgdp-main/mt/oceania_eur.mt')
+j.command(f'gsutil du -sh {dataset_path('gvcf')}')
+j.command(f'gsutil du -sh {dataset_path('cram')}')
+j.command(f'gsutil du -sh {dataset_path('mt/oceania.mt')}')
+j.command(f'gsutil du -sh {dataset_path('mt/oceania_eur.mt')}')
 b.run()
